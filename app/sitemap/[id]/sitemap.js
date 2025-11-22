4c098642b3496cba0b9cb85dd37e8fc7c620ea8a
786mdfaizan <786mdjubair@gmail.com>
2025-11-22 10:16:11 +0530

    update

--- app/sitemap/[id]/sitemap.js
+++ app/sitemap/[id]/sitemap.js
@@ -1,18 +1,20 @@
 import { BASE_URL } from '@/app/lib/constants'
- 
+
 export async function generateSitemaps() {
-  // Fetch the total number of products and calculate the number of sitemaps needed
+  // Total 4 sitemap files
   return [{ id: 0 }, { id: 1 }, { id: 2 }, { id: 3 }]
 }
- 
-export default async function sitemap(props) {
-  const id = await props.id
-  // Google's limit is 50,000 URLs per sitemap
+
+export default async function sitemap({ params }) {
+  const id = Number(params.id)
+
   const start = id * 50000
   const end = start + 50000
+
   const products = await getProducts(
     `SELECT id, date FROM products WHERE id BETWEEN ${start} AND ${end}`
   )
+
   return products.map((product) => ({
     url: `${BASE_URL}/product/${product.id}`,
     lastModified: product.date,
