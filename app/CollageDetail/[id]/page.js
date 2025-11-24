1af1494df853ea62fa574d0898297ebed6ac546b
786mdfaizan <786mdjubair@gmail.com>
2025-11-24 11:58:23 +0530

    u4

--- app/CollageDetail/[id]/page.js
+++ app/CollageDetail/[id]/page.js
@@ -7,7 +7,7 @@ export default function Page({ params }) {
 }
 
 export async function generateMetadata({ params }) {
-  const { collegeId } = await params;
+  const { collegeId } = params;
 
   const API_BASE_URL = process.env.NEXT_PUBLIC_API_URL || 'https://careeraashram-backend.onrender.com';
 console.log(collegeId)
