c6457970dac9fa2444973a9c6377cb0c4a55ff79
786mdfaizan <786mdjubair@gmail.com>
2025-11-24 15:02:01 +0530

    update

--- app/examDetail/[id]/page.js
+++ app/examDetail/[id]/page.js
@@ -3,16 +3,18 @@ import React from 'react'
 
 export async function generateMetadata({ params }) {
 
-  const {id} = params
+  const {id} = await params
 
     return {
       title: ` ${id} Exam 2025: Dates, Pattern, Syllabus & Free PYQ PDFs`,
       description:`Preparing for ${id} 2025? Get official dates, pattern, cut-offs, mock tests & free previous year papers - everything updated for you. `,
       keywords: [id, "exam", "entrance"].join(", "),
 
-
     };
 }
+
+
+
 
 const page = () => {
   return (
