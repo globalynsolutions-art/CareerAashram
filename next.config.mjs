2b8cacd358acbba2f43bc769fc78c5b90e5fb53f
786mdfaizan <786mdjubair@gmail.com>
2025-12-04 13:30:49 +0530

    u2

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,18 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  /* config options here */
-  reactCompiler: true,
+  reactCompiler: true, // ← keep your compiler
+
+  // THIS IS THE ONLY THING YOU NEED TO ADD
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'res.cloudinary.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
